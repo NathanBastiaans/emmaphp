a0f6533f812dd374f26ba92f0f6f3e13c2d2a03f
bobdesaunois <bobdesaunois@gmail.com>
2014-09-11 10:19:22 +0200

    Table progress.

--- system/table.php
+++ system/table.php
@@ -1,12 +1,31 @@
 <?php
 
+/**
+ * Base Table of the EmmaPHP MVC Framework
+ */
 abstract class EmmaTable {
     
-    protected $name;
+    private $_db;
+    protected $table_name;
     
     function __construct() {
     
+        $this->_db = new Db();
         
+    }
+    
+    private function _init() {
+        
+        $sql = "SELECT * FROM :tablename;";
+        $stmt = $this->db->connection->prepare($sql);
+        $stmt->bindParam(":tablename", $this->table_name, PDO::PARAM_STRING);
+        $stmt->execute();
+        
+        $result = $stmt->fetch(PDO::FETCH_CLASS, "EmmaTable");
+        
+        die(var_dump($result));
+        
+//        return $result;
         
     }
     
