87021a24b0d4ebd105164ea704b93ae73c5cd372
bobdesaunois <bobdesaunois@gmail.com>
2014-09-25 14:05:27 +0200

    Moving some data

--- system/database.php
+++ system/database.php
@@ -16,32 +16,4 @@ class Database {
         
     }
     
-//    public function query ($param_query) {
-//
-//        $sql = $param_query;
-//        $stmt = $this->connection->prepare ($sql);
-//        $stmt->execute ();
-//
-//    }
-//
-//    public function fetch ($param_query) {
-//
-//        $sql = $param_query;
-//        $stmt = $this->connection->prepare ($sql);
-//        $stmt->execute ();
-//
-//        return $stmt->fetch (PDO::FETCH_ASSOC);
-//
-//    }
-//
-//    public function fetchAll ($param_query) {
-//
-//        $sql = $param_query;
-//        $stmt = $this->connection->prepare ($sql);
-//        $stmt->execute ();
-//
-//        return $stmt->fetchAll (PDO::FETCH_ASSOC);
-//
-//    }
-    
 }
--- system/model.php
+++ system/model.php
@@ -10,4 +10,32 @@ abstract class EmmaModel {
         
     }
 
+//    public function query ($param_query) {
+//
+//        $sql = $param_query;
+//        $stmt = $this->connection->prepare ($sql);
+//        $stmt->execute ();
+//
+//    }
+//
+//    public function fetch ($param_query) {
+//
+//        $sql = $param_query;
+//        $stmt = $this->connection->prepare ($sql);
+//        $stmt->execute ();
+//
+//        return $stmt->fetch (PDO::FETCH_ASSOC);
+//
+//    }
+//
+//    public function fetchAll ($param_query) {
+//
+//        $sql = $param_query;
+//        $stmt = $this->connection->prepare ($sql);
+//        $stmt->execute ();
+//
+//        return $stmt->fetchAll (PDO::FETCH_ASSOC);
+//
+//    }
+
 }
