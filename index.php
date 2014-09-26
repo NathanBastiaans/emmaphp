c43463477e5d1f1e75581c923e90dd47937208b9
bobdesaunois <bobdesaunois@gmail.com>
2014-09-26 12:12:56 +0200

    1.1.0

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
  * EmmaPHP Model View Controller Framework.
  * @author Bob Desaunois
  * 
- * @version v1.0.0-ALPHA
+ * @version v1.1.0-ALPHA
  */
+
+define ("VERSION", "v1.1.0-ALPHA");
 
 require_once ("config/config.php");
 require_once ("config/database.php");
