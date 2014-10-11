bfe784f790ab7ffbeadeace832eff74465bdc2d8
bobdesaunois <bobdesaunois@gmail.com>
2014-10-11 02:44:30 +0200

    Database basemodel methods
    A set of useful functions in the models,
    this should make stuff a lot easier.

--- system/emmamodel.php
+++ system/emmamodel.php
@@ -5,11 +5,37 @@ abstract class EmmaModel implements Model {
     static $instance;
     
     public $db;
-    
-    function __construct () {
+
+    /**
+     * @todo TEST THESE FUNCTIONS
+     */
+    public function query ($query, $params) {
         
-//        $this->db = new Database ();
+        $stmt = $this->db->connection->prepare ($query);
+        $stmt->execute ($params);
         
     }
-
+    
+    public function fetch ($query, $params) {
+        
+        $stmt = $this->db->connection->prepare ($query);
+        $stmt->execute ($params);
+        $result = $stmt->fetch (PDO::FETCH_ASSOC);
+        $stmt->closeCursor ();
+        
+        return $result;
+        
+    }
+    
+    public function fetchAll ($query, $params) {
+        
+        $stmt = $this->db->connection->prepare ($query);
+        $stmt->execute ($params);
+        $result = $stmt->fetchAll (PDO::FETCH_ASSOC);
+        $stmt->closeCursor ();
+        
+        return $result;
+        
+    }
+    
 }
