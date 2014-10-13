d075b1e38dc9cc1449808d15a95f022c9677653c
bobdesaunois <bobdesaunois@gmail.com>
2014-10-13 11:41:46 +0200

    Removed filter from session setter
    It should be secure as long as POST and GET are filtered.

--- system/session.php
+++ system/session.php
@@ -22,7 +22,7 @@ class Session implements SystemComponent {
 
     public function set ($var_name, $value) {
 
-        $_SESSION[$var_name] = filter_var ($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $_SESSION[$var_name] = $value;
 
     }
 
