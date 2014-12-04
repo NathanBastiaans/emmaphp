83f3718339bad2db0333164a26c919b875100128
bobdesaunois <bobdesaunois@gmail.com>
2014-12-04 16:42:07 +0100

    No Version
    It doesn't make a lot of sense to give the indev version a version
    because it's not genuinely that version.

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
  * EmmaPHP Model View Controller Framework.
  * @author Bob Desaunois
  * 
- * @version v2.0.1-BETA
+ * @version v2.X.X-BETA INDEV
  */
 
-define ("VERSION", "v2.0.1-BETA");
+define ("VERSION", "v2.X.X-BETA INDEV");
 
 require_once ("config/config.php");
 require_once ("config/application_config.php");
