95c042a9c7f88ba414e730283625969706d53bba
bobdesaunois <bobdesaunois@gmail.com>
2015-01-08 13:46:50 +0100

    Versioning

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
  * EmmaPHP Model View Table Controller Framework.
  * @author Bob Desaunois
  * 
- * @version v1.0.3
+ * @version v1.1.0
  */
 
 // Define version
-define ("VERSION", "v1.0.0");
+define ("VERSION", "v1.1.0");
 
 // Include application config
 require_once ("config/application_config.php");
