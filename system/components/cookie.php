e2de9e1639efe73572ed8701df13e7ecbb39c759
bobdesaunois <bobdesaunois@gmail.com>
2015-02-27 13:14:33 +0100

    Cookie System Component
    Untested, but it's there

---
+++ system/components/cookie.php
@@ -0,0 +1,37 @@
+<?php
+
+class Cookie
+{
+    
+    /**
+     * 
+     * @param string $varName
+     * @return Ambigous <boolean, string>
+     */
+    static function get ($varName)
+    {
+
+        return isset ($_COOKIE[$varName]) 
+        	? $_COOKIE[$varName] 
+        	: false;
+
+    }
+
+    /**
+     * Sets a cookie using PHP's setcookie ()
+     * Defaults to expire in a week.
+     * 
+     * @param string 	$varName
+     * @param integer 	$value
+     * @param integer 	$time
+     * @param string 	$secure
+     * @param string 	$httpOnly
+     */
+    static function set ($varName, $value, $time = 604800, $secure = false, $httpOnly = false)
+    {
+
+        setcookie($varName, $value, $time, "/", BASEPATH, $secure, $httpOnly);
+
+    }
+
+}
