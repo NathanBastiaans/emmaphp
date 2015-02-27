26a51a737e872433ee2608ec836125862b4c317c
bobdesaunois <bobdesaunois@gmail.com>
2015-02-27 13:07:34 +0100

    Made all methods of Session component static
    For some reason they weren't.
    I wonder how they worked..

--- system/components/session.php
+++ system/components/session.php
@@ -2,18 +2,8 @@
 
 class Session
 {
-
-    function __construct ()
-    {
-
-        $this->initialize ();
-
-    }
     
-    private function initialize ()
-    {}
-    
-    public function getAndNullify ($var_name)
+    static function getAndNullify ($var_name)
     {
         
         $val = self::get ($var_name);
@@ -22,7 +12,7 @@ class Session
         
     }
 
-    public function get ($var_name)
+    static function get ($var_name)
     {
 
         return isset ($_SESSION[$var_name]) 
@@ -31,14 +21,14 @@ class Session
 
     }
 
-    public function set ($var_name, $value)
+    static function set ($var_name, $value)
     {
 
         $_SESSION[$var_name] = $value;
 
     }
 
-    public function nullify ($var_name)
+    static function nullify ($var_name)
     {
 
         $_SESSION[$var_name] = null;
