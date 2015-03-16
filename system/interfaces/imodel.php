049495958d2d39519c1b7d7cc416c41b181c3591
Bob Desaunois <bobdesaunois@gmail.com>
2015-03-16 12:32:44 +0000

    IModel interface Commentation

--- system/interfaces/imodel.php
+++ system/interfaces/imodel.php
@@ -4,5 +4,25 @@ interface IModel
 {
 
     function constructor ();
+    
+    /**
+     * Accepts a SQL query as a string
+     * and execute it into the database.
+     */
+    function query ();
+    
+    /**
+     * Accepts a SQL query as a string
+     * and executes it into the database.
+     * Afterwards it returns the selected data as a DataObject.
+     */
+    function fetch ();
+    
+    /**
+     * Accepts a SQL query as a string
+     * and executes it into the database.
+     * Afterwards it returns the selected data as an array with DataObjects.
+     */
+    function fetchAll ();
 
 }
